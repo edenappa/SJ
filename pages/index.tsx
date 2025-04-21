9d1e0f1bde44c45abccee3f4f186b1e1e27976bc
edenappa <remon1143@gmail.com>
2025-04-22 01:57:09 +0900

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: 40 }}>
       <h1>SilentService / Shinji</h1>
-      <p>정상 구조로 빌드된 페이지입니다. 이제 출현이 가능합니다.</p>
+      <p>✅ TypeScript 환경 완비 - 이제 빌드는 성공합니다.</p>
     </div>
   );
 }
